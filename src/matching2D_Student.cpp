379d211ad5fe716e0b7ba58187ef43f8909713d8
abdulrahman.hassanin <abdulrahman.hasanin@gmail.com>
2020-01-25 17:41:24 +0200

    Update matching2D_Student.cpp

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -106,6 +106,7 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
     extractor->compute(img, keypoints, descriptors);
     // t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     // cout << descriptorType << " descriptor extraction in " << 1000 * t / 1.0 << " ms" << endl;}
+}
 
 // Detect keypoints in image using the traditional Shi-Thomasi detector
 void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
